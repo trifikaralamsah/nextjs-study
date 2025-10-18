9e94741cac28b1ee7155043cfc9b4e8fdab4ced4
trifikar <trifikar24@gmail.com>
2025-10-18 17:06:21 +0700

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,62 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import antfu from '@antfu/eslint-config'
+import eslintPluginNext from '@next/eslint-plugin-next'
+import eslintPluginQuery from '@tanstack/eslint-plugin-query'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import eslintPluginSonarjs from 'eslint-plugin-sonarjs'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default antfu(
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+    react: true,
+    typescript: true,
 
-export default eslintConfig;
+    lessOpinionated: true,
+    isInEditor: false,
+
+    formatters: {
+      css: 'prettier',
+    },
+
+    ignores: ['*.md'],
+  },
+  {
+    plugins: {
+      '@next/next': eslintPluginNext,
+    },
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs['core-web-vitals'].rules,
+    },
+  },
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  eslintPluginSonarjs.configs.recommended,
+  eslintPluginQuery.configs['flat/recommended'],
+  {
+    rules: {
+      // antfu
+      'antfu/consistent-chaining': 'off',
+      'antfu/no-top-level-await': 'off',
+
+      // node
+      'node/prefer-global/buffer': 'off',
+      'node/prefer-global/process': 'off',
+
+      // ts
+      'ts/no-misused-promises': 'off',
+      'ts/consistent-type-definitions': ['error', 'type'],
+
+      // react
+      'react/no-forward-ref': 'off',
+      'react/no-array-index-key': 'off',
+
+      // next
+      '@next/next/no-img-element': 'off',
+
+      // stylistic
+      'style/semi': ['error', 'never'],
+      'style/quotes': ['error', 'single', { avoidEscape: true }],
+      'style/jsx-sort-props': ['error', { shorthandFirst: true, noSortAlphabetically: true, callbacksLast: true }],
+    },
+  }
+)
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'standalone',
 
-export default nextConfig;
+  // optional: matikan telemetry saat build container
+  telemetry: false,
+}
+
+export default nextConfig
