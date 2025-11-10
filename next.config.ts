07c6ed318ce5df558ae0c5a5bfc24e333368605f
Tri Fikar Alamsah <trifikar@nusantarabetastudio.com>
2025-11-10 11:22:29 +0700

    build(npm): update nextjs

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-
-  // optional: turn off telemetry when build container
-  telemetry: false,
 }
 
 export default nextConfig
