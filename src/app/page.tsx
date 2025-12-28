00bc90bc9c7026c428b5a9de7a9b19a573602ac2
fikar <trifikar24@gmail.com>
2025-12-28 18:14:28 +0700

    feat(app): update wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li className='tracking-[-.01em]'>Save and see your changes instantly.</li>
+          <li className='tracking-[-.01em]'>Learn Next.js 2026.</li>
         </ol>
 
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
